dd26f0289ec68f20f99fc5ad86f4d48c4e8a002d
ryota-amidas <tomizawa.rc@om.asahi-kasei.co.jp>
2025-05-27 14:49:08 +0900

    first

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { CatImage } from "./cat-image"; // 追加
+import { fetchImage } from "./fetch-image";
+ 
+export default async function Home() {
+  // APIから画像を取得
+  const image = await fetchImage();
+  // 画像のURLを渡す
+  return <CatImage url={image.url} />;
+}
